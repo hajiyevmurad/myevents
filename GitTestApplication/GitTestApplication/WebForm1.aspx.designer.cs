ba46f9b6e7a90f247f3253d82b030596c91ab771
Murad <official.haciyev@gmail.com>
2015-09-09 11:01:29 +0500

    Commit 2

--- GitTestApplication/GitTestApplication/WebForm1.aspx.designer.cs
+++ GitTestApplication/GitTestApplication/WebForm1.aspx.designer.cs
@@ -29,5 +29,23 @@ namespace GitTestApplication {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button Button1;
+        
+        /// <summary>
+        /// TextBox1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox1;
+        
+        /// <summary>
+        /// TextBox2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox2;
     }
 }
